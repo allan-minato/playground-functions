60f35a92ff8714896acae3c5fc4a8eda02ef89b2
Allan Medeiros Minato <allan.minato@gmail.com>
2022-09-27 15:14:55 -0300

    resolucao do exercicio1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+
+function compareTrue(param1, param2) {
+  if(param1  && param2){
+     return true
+  }else{
+    return false
+  }
 }
+
 
 // Desafio 2
 function calcArea() {
