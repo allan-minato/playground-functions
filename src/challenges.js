445a9291e73c164c28d2083f523a48911c5c854a
Allan Medeiros Minato <allan.minato@gmail.com>
2022-09-27 17:08:55 -0300

    resoluçao exercicio 4

--- src/challenges.js
+++ src/challenges.js
@@ -21,9 +21,12 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+
+function concatName(array) {
+ return array[array.length-1] +', '+ array[0];
+
 }
+
 
 // Desafio 5
 function footballPoints() {
