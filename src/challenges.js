deda98e96d1fa62731154bf5b290ba5fb7640c71
Allan Medeiros Minato <allan.minato@gmail.com>
2022-09-28 00:51:59 -0300

    resoluçao exercicio 9

--- src/challenges.js
+++ src/challenges.js
@@ -101,11 +101,29 @@ function fizzBuzz(arrNumbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
+
+  function encode(string) {
+    let newString = string;
+    string = string.replace(/a/g, "1")
+    string = string.replace(/e/g, "2")
+    string = string.replace(/i/g, "3")
+    string = string.replace(/o/g, "4")
+    string = string.replace(/u/g, "5")
+    
+   return string;
+  }
+
+
+function decode(string) {
+ 
+    let newString = string;
+    string = string.replace(/1/g, "a")
+    string = string.replace(/2/g, "e")
+    string = string.replace(/3/g, "i")
+    string = string.replace(/4/g, "o")
+    string = string.replace(/5/g, "u")
+    
+   return string;
 }
 
 // Desafio 10
